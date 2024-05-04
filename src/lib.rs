cef6a52feacbb7d533fc3af3e67db806a75968c8
dm <davarlamov810@gmail.com>
2024-05-05 00:34:27 +0300

    feat: improve loops to iterators

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::{env, fs};
+use std::{env, fs, iter};
 
 pub struct Config {
     pub query: String,
@@ -8,12 +8,19 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static String> {
-        if args.len() < 3 {
-            eprintln!("Not enough arguments. You should give atleast 2");
-        }
-        let query = args[1].clone();
-        let file_name = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(query) => query,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_name = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
@@ -41,28 +48,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut vec: Vec<&str> = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            vec.push(line.trim());
-        }
-    }
-
-    vec
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut vec: Vec<&str> = Vec::new();
     let query = query.to_lowercase();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            vec.push(line.trim());
-        }
-    }
-
-    vec
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
